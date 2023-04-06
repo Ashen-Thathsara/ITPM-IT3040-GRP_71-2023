cab6a1d6ebc941ef9835824f7ee39bc4d45511d0
Ashen Thathsara <it20612214@my.sliit.lk>
2023-04-07 00:29:00 +0530

    Create PORT and DB connections

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,11 +1,34 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const bodyParser = require('body-parser');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const dotenv = require("dotenv");
+const cors = require("cors");
 const app = express();
+require("dotenv").config();
 
 const PORT = process.env.PORT || 8070;
 
+dotenv.config();
 app.use(cors());
 app.use(bodyParser.json());
+
+const URL = process.env.MONGODB_URL;
+
+mongoose.connect(URL, {
+    //First 2 comment
+   // useCreateIndex: true,
+    //useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+//Connect Database
+const connection = mongoose.connection;
+connection.once("open", () => {
+    console.log("🚀 DB connected successfully!");
+});
+
+//Connect PORT
+app.listen(PORT, () => {
+    console.log(`💡Server is started on port number: ${PORT}`);
+});
